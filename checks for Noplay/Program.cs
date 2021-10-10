a9a4d9972fa8773560bb7f3ac3d8217e7f255ed2
Jsowmya08 <sowmya.jangam0807@gmail.com>
2021-10-09 23:17:23 -0700

    no play

---
+++ checks for Noplay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace checks_for_Noplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
